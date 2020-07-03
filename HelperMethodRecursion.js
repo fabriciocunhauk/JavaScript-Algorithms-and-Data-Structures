b0f06d59b7d7b918c69f78b669469f86b7069d0d
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-07-03 14:55:08 +0100

    fix

--- HelperMethodRecursion.js
+++ HelperMethodRecursion.js
@@ -3,19 +3,19 @@
 function collectOddValues(arr) {
 
     let result = []
-    console.log(result)
 
     function helper(helperInput) {
         if (helperInput.length === 0) {
-            return
+            return;
         }
-        if (helperInput[0 % 2 !== 0]) {
+        if (helperInput[0] % 2 !== 0) {
             result.push(helperInput[0])
         }
         helper(helperInput.slice(1))
     }
     helper(arr)
-    return result + "empty"
+    console.log(result)
+    return result
 }
 
 collectOddValues([1, 2, 3, 4, 5, 6, 7, 8])
