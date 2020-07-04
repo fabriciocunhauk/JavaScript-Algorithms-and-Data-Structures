1236ff91432d2b11a98c3fd2b2ef9f5242efdb18
Fabricio Cunha <60474481+fabriciocunhauk@users.noreply.github.com>
2020-07-04 02:00:50 +0100

    Update RecursonSumRange.js

--- RecursonSumRange.js
+++ RecursonSumRange.js
@@ -1,4 +1,5 @@
-// Some computer programming languages allow a module or function to call itself. This technique is known as recursion. In recursion, a function α either calls itself directly or calls a function β that in turn calls the original function α. The function α is called recursive function.
+/* Some computer programming languages allow a module or function to call itself. This technique is known as recursion. In recursion, a function α either calls itself directly
+or calls a function β that in turn calls the original function α. The function α is called recursive function.*/
 
 function sumRange(num) {
     if (num === 1) return 1
