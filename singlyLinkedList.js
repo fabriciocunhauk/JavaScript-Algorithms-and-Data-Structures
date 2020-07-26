386b3767adfd72ebb341e171a070107be05324b2
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-07-26 03:25:54 +0100

    SinglyLinkedList complete phiwww

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -18,8 +18,8 @@ class SinglyLinkedList {
     push(val) {
         var newNode = new Node(val)
         if (!this.head) {
-            this.head = newNode
-            this.tail = this.head
+            this.head = newNode;
+            this.tail = this.head;
         } else {
             this.tail.next = newNode;
             this.tail = newNode;
@@ -33,8 +33,8 @@ class SinglyLinkedList {
         var current = this.head;
         var newTail = current;
         while (current.next) {
-            newTail = current
-            current = current.next
+            newTail = current;
+            current = current.next;
         }
         this.tail = newTail;
         this.tail.next = null;
@@ -44,6 +44,84 @@ class SinglyLinkedList {
             this.tail = null;
         }
         return current
+    }
+    shift() {
+        if (!this.head) return undefined;
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        return currentHead;
+    }
+    unshift(val) {
+        var newNode = new Node(val)
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+        var counter = 0;
+        var current = this.head;
+        while (counter !== index) {
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+    set(index, val) {
+        var foundNode = this.get(index);
+        if (foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === this.length) return !!this.push(val);
+        if (index === 0) return !!this.unshift(val);
+
+        var newNode = new Node(val);
+        var previous = this.get(index - 1);
+        var temp = previous.next;
+        previous.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+
+        var previousNode = this.get(index - 1);
+        var removed = previousNode.next;
+        previousNode.next = removed.next;
+        this.length--;
+        return removed;
+    }
+    reverse() {
+        var node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        var next;
+        var prev = null;
+        for (var i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
     }
 }
 
